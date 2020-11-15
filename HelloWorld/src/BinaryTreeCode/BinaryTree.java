ca4e5855591b3d3107f3f7240143aa180ea1c65f
Admin <Admin@192.168.60.19>
2020-11-15 22:40:26 +0000

    updated BinaryTreeCode

---
+++ HelloWorld/src/BinaryTreeCode/BinaryTree.java
@@ -0,0 +1,16 @@
+package BinaryTreeCode;
+
+public class BinaryTree {
+	Node root;
+	
+	BinaryTree(int key){
+		root = new Node(key);
+		
+	}
+	
+	BinaryTree(){
+		root = null;
+	}
+	
+
+}
---
+++ HelloWorld/src/BinaryTreeCode/Node.java
@@ -0,0 +1,14 @@
+package BinaryTreeCode;
+import java.util.*;
+
+public class Node {
+	int key;
+	Node left, right;
+	
+	
+	public Node(int item) {
+		key=item;
+		left = right = null;
+	}
+
+}
