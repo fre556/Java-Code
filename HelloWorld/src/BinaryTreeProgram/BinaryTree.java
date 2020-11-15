01337e6b26465de7ac07a90484e6f537cc788779
Admin <Admin@192.168.60.19>
2020-11-15 22:28:21 +0000

    updated binary tree

--- HelloWorld/src/BinaryTreeProgram/BinaryTree.java
+++ HelloWorld/src/BinaryTreeProgram/BinaryTree.java
@@ -1,22 +1,50 @@
 package BinaryTreeProgram;
-
-public class BinaryTree {
+import java.util.*;
+class BinaryTree {
 	//root
-	Node root;
+	private Node root;
 	
-	//constructor
-	BinaryTree(int value)
-	{
-		root = new Node(value);
-	}
-	
-	BinaryTree(int value){
-		
-		
+	//create a binary tree with root node
+	public BinaryTree(Node root) {
+		this.setRoot(root);
 	}
 	
 	
+	//create empt tree
+	public BinaryTree() {
+		this.setRoot(null);
+	}
 	
+	public Node getRoot() {
+		return root;
+	}
+	
+	public void setRoot(Node root) {
+		this.root = root;
+	}
+	
+	
+	public void insert(Node newNode) {
+		if (this.root == null) {
+			this.root = newNode;
+		}
+		else if (this.root.getLeft() == null) {
+			this.root.setLeft(newNode);
+		}
+		else if (this.root.getRight() ==null) {
+			this.root.setRight(newNode);
+		}
+		else {
+			List<Node> siblingNodes = new LinkedList<Node>();
+			siblingsNodes.add(this.root.getLeft());
+			siblingsNodes.add(this.root.getRight());
+			insert2(siblingsNode,newNode);
+		}
+		
+	}
+	
+	
+	//https://github.com/jackychu0830/binarytree/blob/master/src/main/java/com/jackychu/app/binary_tree/BinaryTree.java
 	
 
 }
