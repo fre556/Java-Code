fe711b91cefa889489d63aada624f1371b7682cf
Admin <Admin@DESKTOP-NEIJAP6>
2020-11-18 17:17:31 +0000

    Renamed

---
+++ HelloWorld/src/LinkedList2/LinkedListTest.java
@@ -0,0 +1,34 @@
+package LinkedList2;
+import java.util.*;
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		// Creating object of the 
+        // class linked list 
+        LinkedList<String> ll 
+            = new LinkedList<String>(); 
+        
+        LinkedList<String> l2 = new LinkedList<String>();
+  
+        // Adding elements to the linked list 
+        ll.add("A"); 
+        ll.add("B"); 
+        ll.addLast("C"); 
+        ll.addFirst("D"); 
+        ll.add(2, "E"); 
+  
+        System.out.println(ll); 
+  
+        ll.remove("B"); 
+        ll.remove(3); 
+        ll.removeFirst(); 
+        ll.removeLast(); 
+        
+        l2.add("k");
+        l2.add("x");
+        
+        l1.head = new
+        System.out.println(ll);
+	}
+
+}
